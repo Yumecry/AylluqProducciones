9d6ab7fa68112c51055de765b6ea82410b4bff11
Yumecry <jesustupro_pc2016@hotmail.com>
2026-06-22 04:44:49 -0500

    cambiando importacion de componente next -> react

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Portfolio from './components/Portfolio';
 import Packages from './components/Packages';
 import Footer from './components/Footer';
 
-import { Analytics } from '@vercel/analytics/next';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App() {
   const [currentView, setCurrentView] = useState('home'); // 'home' or 'packages'
